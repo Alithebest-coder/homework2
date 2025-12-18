de07a97a8e13ee76b9be9b290b62488f624c4abc
bob <aurumprint@proton.me>
2025-12-18 18:23:30 +1000

    Add files via upload

---
+++ what.py
@@ -0,0 +1,6 @@
+set1={1,2,3,4,5,6,7,8,9,10}
+set2={5,6,7,8,9,10,11,12,13,14}
+set3=set1.symmetric_difference(set2)
+print("set1:",set1)
+print("set2:",set2)
+print("symmetric difference (set3):",set3)
